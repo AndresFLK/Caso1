8f1168e122068a64646847b17996177ec0569f44
AndresFLK <andrescrptycol@gmail.com>
2024-02-25 16:51:43 -0600

    Agregar archivos de proyecto.

---
+++ API/Program.cs
@@ -0,0 +1,43 @@
+using Aplication;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+const string LOCAL_HOST_CORS = "Web";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Web", policy =>
+    {
+        policy.WithOrigins("http://localhost:8001","https://localhost:44301")
+            .WithHeaders("*")
+            .WithMethods("*");
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAplicationServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(LOCAL_HOST_CORS);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
