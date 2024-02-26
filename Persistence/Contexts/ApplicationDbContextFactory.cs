4e9d750c616f87e232ab0c536fbf665d9519c81a
ElectroMilitary <dereklevilla45@gmail.com>
2024-02-25 18:47:18 -0600

    Create ApplicationDbContextFactory.cs

---
+++ Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Persistence.Contexts;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=ufide;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True");
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
